01a1dce109253b48956df0d980cbb8d618170eef
tomas.najnar <tomas.najnar@saholding.sa.priv>
2023-02-10 21:16:22 +0100

    set tomorrow condition

--- src/components/ui/CarRepairSign.tsx
+++ src/components/ui/CarRepairSign.tsx
@@ -18,9 +18,21 @@ const tomorrow = new Date(today.getTime() + 24 * 60 * 60 * 1000);
 const tomorrowTimestamp = Timestamp.fromDate(tomorrow);
 
 const CarRepairSign = ({ carRepairDate }: ICarRepairSignProps) => {
-  const [showDialog, setShowDialog] = useState<boolean>(false);
+  let carRepairTomorrowDateTimestamp;
 
-  // console.log(tomorrowTimestamp.toDate().toISOString().substring(0, 10));
+  const carTomorrow = carRepairDate
+    ? new Date(carRepairDate.toMillis() + 24 * 60 * 60 * 1000)
+    : undefined;
+
+  if (carTomorrow) {
+    carRepairTomorrowDateTimestamp = Timestamp.fromDate(carTomorrow);
+  }
+
+  console.log(
+    carRepairTomorrowDateTimestamp?.seconds,
+    " || ",
+    todayTimestamp.seconds
+  );
 
   return (
     <div>
@@ -28,9 +40,12 @@ const CarRepairSign = ({ carRepairDate }: ICarRepairSignProps) => {
         className={clsx(
           "absolute left-1 top-1 w-3 h-3 rounded-full",
           !carRepairDate && "hidden",
-          carRepairDate &&
-            carRepairDate?.seconds > tomorrowTimestamp.seconds &&
+          carRepairTomorrowDateTimestamp?.seconds === todayTimestamp.seconds &&
             "hidden",
+          // carRepairDate &&
+          //   carRepairDate?.seconds + 24 * 60 * 60 * 1000 ===
+          //     tomorrowTimestamp.seconds &&
+          //   "hidden",
           carRepairDate?.seconds !== todayTimestamp.seconds && "bg-red-600",
           carRepairDate?.seconds === todayTimestamp.seconds && "bg-green-600"
         )}
