47ff7ad025a3b8f95b5ad1b321c2462da41fa3a6
tomas.najnar <tomas.najnar@saholding.sa.priv>
2023-02-10 13:50:03 +0100

    css to tailwind

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,18 +1,20 @@
+import { useContext } from "react";
 import Locomotive from "../components/Train/Locomotive";
 import Vehicle from "../components/Train/Vehicle";
+import { KeycloakContext } from "../helpers/KeycloakContext";
 
 const HomePage = () => {
+  const { keycloakValue } = useContext(KeycloakContext);
+
   return (
     <div>
+      <div className="text-center text-h2 font-bold">
+        Vítej {keycloakValue?.idTokenParsed?.preferred_username}
+      </div>
       <h1 className="text-2xl pb-6">Instrukce do aplikace</h1>
-      <div className="flex pb-10 gap-6">
-        Lokomotiva =
-        {/* <Locomotive /> */}
-      </div>
-      <div className="flex gap-14">
-        Vagon =
-        {/* <Vehicle /> */}
-      </div>
+
+      <div className="flex pb-10 gap-6">Lokomotiva ={/* <Locomotive /> */}</div>
+      <div className="flex gap-14">Vagon ={/* <Vehicle /> */}</div>
     </div>
   );
 };
